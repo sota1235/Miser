241ffede046b315f526146bd07d54d2febe5869c
sota1235 <sota1235@gmail.com>
2016-11-17 16:54:50 +0900

    fix: namespace for app

--- webroot/index.php
+++ webroot/index.php
@@ -8,7 +8,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use Dietcube\Dispatcher;
 
 Dispatcher::invoke(
-    '\\YourProject\\Application',
+    '\\Miser\\Application',
     dirname(__DIR__) . '/app',
     Dispatcher::getEnv()
 );
