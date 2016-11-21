0a806b3b05967147cfa873c820f731d7f96fc131
sota1235 <sota1235@gmail.com>
2016-11-21 20:50:07 +0900

    modify: mount calenra component with dummy data

--- resources/js/components/app-component.js
+++ resources/js/components/app-component.js
@@ -2,10 +2,30 @@
  * @fileoverview Route component for app.
  */
 import React    from 'react';
+import Calendar from 'components/calendar-component';
+import { uniqueId } from 'lodash';
+
+const dummyData = [
+  { day: 26, isCurrent: false, fulldate: uniqueId() },
+  { day: 27, isCurrent: false, fulldate: uniqueId()  },
+  { day: 28, isCurrent: false, fulldate: uniqueId()  },
+  { day: 29, isCurrent: false, fulldate: uniqueId()  },
+  { day: 30, isCurrent: false, fulldate: uniqueId()  },
+  { day: 1, isCurrent: true, fulldate: uniqueId()  },
+  { day: 2, isCurrent: true, fulldate: uniqueId()  },
+  { day: 3, isCurrent: true, fulldate: uniqueId()  },
+  { day: 4, isCurrent: true, fulldate: uniqueId()  },
+  { day: 5, isCurrent: true, fulldate: uniqueId()  },
+  { day: 6, isCurrent: true, fulldate: uniqueId()  },
+  { day: 7, isCurrent: true, fulldate: uniqueId()  },
+  { day: 8, isCurrent: true, fulldate: uniqueId()  },
+  { day: 9, isCurrent: true, fulldate: uniqueId()  },
+];
 
 const App = () => (
   <div className="miser-app">
     <p>Miser</p>
+    <Calendar calendars={ dummyData } />
   </div>
 );
 
