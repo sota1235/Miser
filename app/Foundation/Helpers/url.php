93201df830b4e6d5cd779b1c826f80dc7d4a9b2d
sota1235 <sota1235@gmail.com>
2016-11-21 18:56:13 +0900

    modify: autoload helper class

--- app/Foundation/Helpers/url.php
+++ app/Foundation/Helpers/url.php
@@ -3,3 +3,14 @@
 /**
  * @copyright (c) sota1235
  */
+
+/**
+ * 指定された年月日のカレンダーを表示するページへリダイレクト
+ *
+ * @param int  $year
+ * @param int  $month
+ */
+function redirectToCalendar($year, $month)
+{
+    return "/?year=${year}&month=${month}";
+}
