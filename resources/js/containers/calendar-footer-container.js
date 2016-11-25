4d3d0bbcbc40acbb71613dea529ffc687dec1061
sota1235 <sota1235@gmail.com>
2016-11-25 18:01:35 +0900

    add: container class for calendar footer component

---
+++ resources/js/containers/calendar-footer-container.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview Container for calendar-footer-component.
+ */
+import {connect} from 'react-redux';
+import CalendarFooterComponent from 'components/calendar-footer-component';
+import {goToPrevMonth, goToNextMonth} from 'actions/actions';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClickPrev() {
+    dispatch(goToPrevMonth(ownProps.prevYear, ownProps.prevMonth)
+  },
+  onClickNext() {
+    dispatch(goToPrevMonth(ownProps.nextYear, ownProps.nextMonth)
+  },
+});
+
+const CalendarFooterContainer = connect(mapStateToProps, mapDispatchToProps)(CalendarFooterComponent);
+
+export default CalendarFooterComponent;
