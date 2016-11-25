bee5c0be8ef73953806795c17ce3aa712813334a
sota1235 <sota1235@gmail.com>
2016-11-25 14:40:49 +0900

    modify: implement service class

--- tests/Service/CalendarServiceTest.php
+++ tests/Service/CalendarServiceTest.php
@@ -2,13 +2,17 @@
 
 namespace Miser\Service;
 
-use Carbon\Carbon;
-use CalendR\Calendar;
-use CalendR\Period\Month;
+use Mockery as m;
+use Mockery\MockInterface as i;
 use Miser\Service\CalendarService;
+use Miser\Entities\CalendarDayEntity;
+use Miser\Repositories\CalendarRepositoryInterface;
 
 class CalendarServiceTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var i|CalendarRepositoryInterface */
+    protected $calendar;
+
     /** @var CalenderService */
     protected $service;
 
@@ -16,16 +20,52 @@ class CalendarServiceTest extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
 
-        $this->service = new CalendarService(new Calendar);
+        $this->calendar = m::mock(CalendarRepositoryInterface::class);
+
+        $this->service = new CalendarService($this->calendar);
     }
 
-    public function testShouldReturnCalendarObject()
+    public function testGetCurrentMonthShouldReturnArray()
     {
-        $this->assertInstanceOf(Month::class, $this->service->getCurrentMonth());
+        $expected = [
+            ['day' => 26, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 27, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 28, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 29, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 30, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 1,  'isCurrent' => true,  'timestamp' => 1235],
+            ['day' => 2,  'isCurrent' => true,  'timestamp' => 1235],
+        ];
+        // make mock response
+        $entities = [];
+        foreach ($expected as $expect) {
+            $entities[] = new CalendarDayEntity($expect['day'], $expect['isCurrent'], $expect['timestamp']);
+        }
+
+        $this->calendar->shouldReceive('getCurrentMonthDays')->andReturn($entities);
+
+        $this->assertEquals($this->service->getCurrentMonth(), $expected);
     }
 
-    public function testGetMonthShouldReturnCalendarObject()
+    public function testGetMonthShouldReturnArray()
     {
-        $this->assertInstanceOf(Month::class, $this->service->getMonth(2016, 9));
+        $expected = [
+            ['day' => 26, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 27, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 28, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 29, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 30, 'isCurrent' => false, 'timestamp' => 1235],
+            ['day' => 1,  'isCurrent' => true,  'timestamp' => 1235],
+            ['day' => 2,  'isCurrent' => true,  'timestamp' => 1235],
+        ];
+        // make mock response
+        $entities = [];
+        foreach ($expected as $expect) {
+            $entities[] = new CalendarDayEntity($expect['day'], $expect['isCurrent'], $expect['timestamp']);
+        }
+
+        $this->calendar->shouldReceive('getMonthDays')->andReturn($entities);
+
+        $this->assertEquals($this->service->getMonth(2016, 10), $expected);
     }
 }
