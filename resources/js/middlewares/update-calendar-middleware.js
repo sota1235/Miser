c5d0066a868a75e96cf58b4cf2d3bf69b10c5647
sota1235 <sota1235@gmail.com>
2016-11-25 18:54:18 +0900

    modfify: implement getting data from server on middleware

--- resources/js/middlewares/update-calendar-middleware.js
+++ resources/js/middlewares/update-calendar-middleware.js
@@ -3,13 +3,13 @@
  */
 import * as ActionNames from 'actions/action-types';
 import {updateCalendar} from 'actions/actions';
+import getCalendar from 'webutils/ajax/get-calendar';
 
-const updateCalendarMiddleware = store => next => action => {
+const updateCalendarMiddleware = store => next => async action => {
   if (action.type === ActionNames.GO_TO_PREV_MONTH
     || action.type === ActionNames.GO_TO_NEXT_MONTH) {
-    // TODO: get data form server
     const {year, month} = action;
-    const calendar = [];
+    const calendar = await getCalendar(year, month);
     store.dispatch(updateCalendar(calendar));
   }
 
