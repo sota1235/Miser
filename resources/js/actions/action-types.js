33c0e019cbb5c5a18b3058cc4a41fe40f346d5e4
sota1235 <sota1235@gmail.com>
2016-11-25 17:52:38 +0900

    modify: add action names

--- resources/js/actions/action-types.js
+++ resources/js/actions/action-types.js
@@ -3,5 +3,13 @@
  */
 
 /**
- * @description
+ * @description Display the previous month.
+ * @type {string}
  */
+export const GO_TO_PREV_MONTH = 'GO_TO_PREV_MONTH';
+
+/**
+ * @description Display the next month.
+ * @type {string}
+ */
+export const GO_TO_NEXT_MONTH = 'GO_TO_NEXT_MONTH';
