3fed8d6be356596e09028228320cd85fedb21303
sota1235 <sota1235@gmail.com>
2016-11-26 23:03:23 +0900

    modfiy: get initial data of calendar from server when component did mounted

--- resources/js/containers/calendar-container.js
+++ resources/js/containers/calendar-container.js
@@ -2,10 +2,30 @@
  * @fileoverview Container for calendar-footer-component.
  */
 import {connect} from 'react-redux';
+import React from 'react';
+import getCalendar from 'webutils/ajax/get-calendar';
+import {updateCalendar} from 'actions/actions';
 import CalendarComponent from 'components/calendar-component';
 
 const mapStateToProps = state => ({ calendars: state.calendar });
 
-const CalendarContainer = connect(mapStateToProps)(CalendarComponent);
+class CalendarContainer extends React.Component {
+  componentDidMount() {
+    getCalendar()
+      .then(calendar => {
+        this.props.dispatch(updateCalendar(calendar));
+      });
+  }
 
-export default CalendarContainer;
+  render() {
+    const {calendars} = this.props;
+
+    return (
+      <CalendarComponent
+        calendars={calendars}
+      />
+    );
+  }
+}
+
+export default connect(mapStateToProps)(CalendarContainer);
--- resources/js/reducers/calendar.js
+++ resources/js/reducers/calendar.js
@@ -4,23 +4,6 @@
 import {uniqueId} from 'lodash';
 import * as ActionNames from 'actions/action-types';
 
-const dummyData = [
-  { day: 26, isCurrent: false, timestamp: 1 },
-  { day: 27, isCurrent: false, timestamp: 2 },
-  { day: 28, isCurrent: false, timestamp: 3  },
-  { day: 29, isCurrent: false, timestamp: 4  },
-  { day: 30, isCurrent: false, timestamp: 5  },
-  { day: 1, isCurrent: true, timestamp: 6 },
-  { day: 2, isCurrent: true, timestamp: 7 },
-  { day: 3, isCurrent: true, timestamp: 8 },
-  { day: 4, isCurrent: true, timestamp: 9 },
-  { day: 5, isCurrent: true, timestamp: 10 },
-  { day: 6, isCurrent: true, timestamp: 11 },
-  { day: 7, isCurrent: true, timestamp: 12 },
-  { day: 8, isCurrent: true, timestamp: 13 },
-  { day: 9, isCurrent: true, timestamp: 14},
-];
-
 /**
  * @description Change state of the data of the day.
  * @param {Object} state
@@ -39,7 +22,7 @@ const day = (state, action) => ({
  * @param {Object} action
  * @return {array}
  */
-const calendar = (state = dummyData, action) => {
+const calendar = (state = [], action) => {
   switch (action.type) {
     case ActionNames.UPDATE_CALENDAR:
       return action.calendar;
