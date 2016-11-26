4c2855eaff26534a66ee863df65445212df9d040
sota1235 <sota1235@gmail.com>
2016-11-27 00:22:11 +0900

    modify: fix to update data on reducer

--- resources/js/reducers/current.js
+++ resources/js/reducers/current.js
@@ -9,8 +9,38 @@ const currentDate = moment();
 
 const initialState = {
   year  : currentDate.year(),
-  month : currentDate.month(),
+  month : currentDate.month() + 1,
 };
+
+/**
+ * @description Get data of next month from current date.
+ * @param {number} year
+ * @param {number} month
+ * @return {Object}
+ */
+function getNextMonth(year, month) {
+  const currentDate = moment(`${year}-${month}`, 'YYYY-MM');
+  const nextMonth = currentDate.add(1, 'M');
+  return {
+    year  : nextMonth.year(),
+    month : nextMonth.month() + 1,
+  };
+}
+
+/**
+ * @description Get data of previous month from current date.
+ * @param {number} year
+ * @param {number} month
+ * @return {Object}
+ */
+function getPrevMonth(year, month) {
+  const currentDate = moment(`${year}-${month}`, 'YYYY-MM');
+  const prevMonth = currentDate.subtract(1, 'M');
+  return {
+    year  : prevMonth.year(),
+    month : prevMonth.month() + 1,
+  };
+}
 
 /**
  * @description Make state of current calendar.
@@ -21,14 +51,16 @@ const initialState = {
 const currentCalendar = (state = initialState, action) => {
   switch (action.type) {
     case GO_TO_PREV_MONTH:
+      const prevMonth = getPrevMonth(state.year, state.month);
       return {
-        year  : action.year,
-        month : action.month
+        year  : prevMonth.year,
+        month : prevMonth.month,
       };
     case GO_TO_NEXT_MONTH:
+      const nextMonth = getNextMonth(state.year, state.month);
       return {
-        year  : action.year,
-        month : action.month
+        year  : nextMonth.year,
+        month : nextMonth.month,
       };
     default:
       return state;
