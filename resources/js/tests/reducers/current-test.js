1604a171849fbdbb5096582c07a32ffb6ada76a0
sota1235 <sota1235@gmail.com>
2016-11-27 22:00:48 +0900

    add: unit test for current reducer

---
+++ resources/js/tests/reducers/current-test.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview Unit test for reducers/current
+ */
+import assert from 'assert';
+import currentCalendar from 'reducers/current';
+import {GO_TO_PREV_MONTH, GO_TO_NEXT_MONTH} from 'actions/action-types';
+
+describe('Unit test for reducers/current', () => {
+  it('Shold return prev month with GO_TO_PREV_MONTH action', () => {
+    const expected = {
+      year  : 2016,
+      month : 12,
+    };
+
+    assert.deepEqual(
+      expected,
+      currentCalendar({
+        year  : 2017,
+        month : 1,
+      }, {
+        type: GO_TO_PREV_MONTH,
+      })
+    );
+  });
+
+  it('Shold return next month with GO_TO_NEXT_MONTH action', () => {
+    const expected = {
+      year  : 2017,
+      month : 1,
+    };
+
+    assert.deepEqual(
+      expected,
+      currentCalendar({
+        year  : 2016,
+        month : 12,
+      }, {
+        type: GO_TO_NEXT_MONTH,
+      })
+    );
+  });
+});
