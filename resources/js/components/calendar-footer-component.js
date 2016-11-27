d6e6eab476021b3a3d3d61ab13c571edcacd49b7
sota1235 <sota1235@gmail.com>
2016-11-27 21:46:44 +0900

    modify: use english instead of japanese

--- resources/js/components/calendar-footer-component.js
+++ resources/js/components/calendar-footer-component.js
@@ -6,10 +6,10 @@ import React, { PropTypes } from 'react';
 const CalendarFooter = ({ onClickPrev, onClickNext }) => (
   <div className="level calendar-bar">
     <div className="level-left button is-primary is-outlined" onClick={onClickPrev}>
-      前の月
+      Prev month
     </div>
     <div className="level-right button is-primary is-outlined" onClick={onClickNext}>
-      次の月
+      Next month
     </div>
   </div>
 );
