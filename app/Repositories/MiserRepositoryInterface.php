82952f88b130cddd454f5805cae8c1995919d0ad
sota1235 <sota1235@gmail.com>
2016-12-30 00:56:26 +0900

    add: interface for controlling miser data

---
+++ app/Repositories/MiserRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright (c) sota1235
+ */
+
+namespace Miser\Repositories;
+
+/**
+ * Interface MiserRepositoryInterface
+ */
+interface MiserRepositoryInterface
+{
+    /**
+     * @param string  $pageName
+     * @param int     $year
+     * @param int     $month
+     * @return \Miser\Entities\MiserEntity[]
+     */
+    public function getMisers(string $pageName, int $year, int $month);
+
+    /**
+     * @param string  $pageName
+     * @param int     $year
+     * @param int     $month
+     * @param int     $day
+     * @param bool    $status
+     * @return bool
+     */
+    public function addMiser(string $pageName, int $year, int $month, int $day, bool $status);
+}
