ce9b4a1072e456da3690be17da61b7008665a099
sota1235 <sota1235@gmail.com>
2016-12-30 02:05:31 +0900

    add: repository class for data of misers

---
+++ app/Repositories/MiserRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @copyright (c) sota1235
+ */
+
+namespace Miser\Repositories;
+
+use Miser\Entities\MiserEntity;
+use Miser\DataAccess\Fluent\Miser;
+use Dietcube\Components\LoggerAwareTrait;
+
+/**
+ * Class MiserRepository
+ */
+class MiserRepository implements MiserRepositoryInterface
+{
+    use LoggerAwareTrait;
+
+    /** @var Miser */
+    protected $miser;
+
+    /**
+     * @param Miser  $miser
+     */
+    public function __construct(Miser $miser)
+    {
+        $this->miser = $miser;
+    }
+
+    /**
+     * @param string  $pageName
+     * @param int     $year
+     * @param int     $month
+     * @return \Miser\Entities\MiserEntity[]
+     */
+    public function getMisers(string $pageName, int $year, int $month)
+    {
+        try {
+            $misers = $this->miser->getMisers($pageName, $year, $month);
+        } catch (\Exception $e) {
+            $this->logger->log($e->getMessage());
+            $misers = [];
+        }
+
+        return $misers;
+    }
+
+    /**
+     * @param string  $pageName
+     * @param int     $year
+     * @param int     $month
+     * @param int     $day
+     * @param bool    $status
+     * @return bool
+     */
+    public function addMiser(string $pageName, int $year, int $month, int $day, bool $status)
+    {
+        // TODO: implement
+    }
+}
