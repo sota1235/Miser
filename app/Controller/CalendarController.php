09f4a5b3c23b1826ba41447a9ea5a2673304b6d3
sota1235 <sota1235@gmail.com>
2017-04-25 00:34:16 +0900

    modify: add document of the calendar

--- app/Controller/CalendarController.php
+++ app/Controller/CalendarController.php
@@ -10,6 +10,31 @@ use Dietcube\Controller;
 
 class CalendarController extends Controller
 {
+    /**
+     * @api {get} /api/calendar Get calendar data.
+     * @apiGroup Calendar
+     *
+     * @apiParam {Number} [year] Number of the year.
+     * @apiParam {Number} [month] Number of the month.
+     *
+     * @apiSuccess {Number} day Date.
+     * @apiSuccess {Boolean} isCurrent Is the day current day.
+     * @apiSuccess {Number} timestamp Unix timestamp of day.
+     *
+     * @apiSuccessExample {json} Success Response:
+     *  [
+     *      {
+     *          "day": 27,
+     *          "isCurrent": false,
+     *          "timestamp": 1490572800
+     *      },
+     *      {
+     *          "day": 28,
+     *          "isCurrent": false,
+     *          "timestamp": 1490659200
+     *      }
+     *  ]
+     */
     public function index()
     {
         $year = $this->query('year', null);
